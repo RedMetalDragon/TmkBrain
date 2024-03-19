1da4cd3850bcff9942bf01739e6a3ba5d4d641bb
supersaaam <70166392+supersaaam@users.noreply.github.com>
2024-03-19 10:51:27 -0400

    UPDATE employee-schedule assignment

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -32,16 +32,22 @@ router.get("/users", UsersRestHandler.getEmployees);
 // Employee data
 router.get("/users/:employee_id", UsersRestHandler.getEmployeeData);
 
-// Scheduling and assignment
+// Schedule CRUD
 router.post("/schedule", SchedulesRestHandler.createSchedule);
 router.get("/schedule", SchedulesRestHandler.listSchedule);
 router.get("/schedule/:schedule_id", SchedulesRestHandler.readSchedule);
 router.delete("/schedule/:schedule_id", SchedulesRestHandler.deleteSchedule);
 router.put("/schedule/:schedule_id", SchedulesRestHandler.updateSchedule);
 
+// Employee schedule assignment CRUD
 router.post("/employee-schedules", SchedulesRestHandler.assignSchedule);
 router.get("/employee-schedules", SchedulesRestHandler.getEmployeeSchedule);
 router.get("/employee-schedules/:employee_schedule_id", SchedulesRestHandler.getEmployeeScheduleById);
 router.delete("/employee-schedules/:employee_schedule_id", SchedulesRestHandler.deleteEmployeeScheduleById);
+router.put("/employee-schedules/:employee_schedule_id", SchedulesRestHandler.updateEmployeeScheduleById);
+
+// Get weekly and monthly schedule by ID
+router.get("/employees/:employee_id/weekly-schedule", SchedulesRestHandler.getEmployeeWeeklySchedule);
+// router.get("/employee/:employee_id/monthly-schedule", SchedulesRestHandler.getEmployeeScheduleMonthly);
 
 export { router };
