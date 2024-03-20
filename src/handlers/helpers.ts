b00899272f9f043cb3a86991be5b1d0aee6eeeff
supersaaam <70166392+supersaaam@users.noreply.github.com>
2024-03-20 10:25:00 -0400

    GET employee-weekly/monthly schedule

--- src/handlers/helpers.ts
+++ src/handlers/helpers.ts
@@ -38,3 +38,81 @@ export function findDuplicates(array1: Array<string>, array2: Array<string>): Ar
 
   return duplicates.sort();
 }
+
+export function getWeekDates(): string[] {
+  const today = new Date();
+  const dayOfWeek = today.getDay();
+  const startDate = new Date(today);
+  startDate.setDate(today.getDate() - dayOfWeek); // Set start date to Sunday of the current week
+
+  const dates: string[] = [];
+  for (let i = 0; i < 7; i++) {
+    const date = new Date(startDate);
+    date.setDate(startDate.getDate() + i);
+    const formattedDate = date.toISOString().slice(0,10); // Format as yyyy-MM-dd
+    dates.push(formattedDate);
+  }
+
+  return dates;
+
+}
+
+export function getMonthDates(): string[] {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth();
+  const endDate = new Date(year, month + 1, 0);
+
+  const dates: string[] = [];
+  for (let i = 1; i <= endDate.getDate(); i++) {
+    const date = new Date(year, month, i);
+    const formattedDate = date.toISOString().slice(0,10); // Format as yyyy-MM-dd
+    dates.push(formattedDate);
+  }
+
+  return dates;
+} 
+
+export function getWeekdaysOfMonth(): string[] {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth();
+  const startDate = new Date(year, month, 1);
+  const endDate = new Date(year, month + 1, 0);
+
+  const weekdays: string[] = [];
+  for (let i = 0; i < endDate.getDate(); i++) {
+    const date = new Date(startDate);
+    date.setDate(startDate.getDate() + i);
+    const dayOfWeek = date.getDay();
+    if (dayOfWeek >= 2 && dayOfWeek <= 6) { // Monday to Friday
+      const formattedDate = date.toISOString().slice(0,10); // Format as yyyy-MM-dd
+      weekdays.push(formattedDate);
+    }
+  }
+
+  return weekdays;
+}
+
+export function getWeekdays(): string[] {
+  const today = new Date();
+  const dayOfWeek = today.getDay();
+  const startDate = new Date(today);
+  
+  // Adjust startDate to the previous Sunday
+  startDate.setDate(today.getDate() - dayOfWeek);
+
+  const dates: string[] = [];
+  for (let i = 0; i < 7; i++) {
+    const date = new Date(startDate);
+    date.setDate(startDate.getDate() + i);
+    const dayOfWeek = date.getDay();
+    // Only include weekdays (Monday to Friday)
+    if (dayOfWeek !== 0 && dayOfWeek !== 6) {
+      const formattedDate = date.toISOString().slice(0,10); // Format as yyyy-MM-dd
+      dates.push(formattedDate);
+    }
+  }
+
+  return dates;
+}
