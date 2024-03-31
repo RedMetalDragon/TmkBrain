a0a535c14d879a43068564ef96c3b10e65bd5304
supersaaam <70166392+supersaaam@users.noreply.github.com>
2024-03-31 12:04:21 -0400

    addl feat: Employee-Attendance Compute (cron)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,8 @@ import {
   HealthCheckRestHandler,
   BannersRestHandler,
   UsersRestHandler,
-  SchedulesRestHandler
+  SchedulesRestHandler,
+  AttendanceRestHandler
 } from "../handlers";
 
 const router = Router();
@@ -23,6 +24,7 @@ router.get("/banners", BannersRestHandler.getBanners);
 // Punch in/out
 router.post("/employees/:employee_id/attendance", UsersRestHandler.attendance);
 router.get("/employees/:employee_id/logs", UsersRestHandler.getLogs);
+router.post("/employees/attendance/compute", AttendanceRestHandler.computeAttendance);
 
 // Attendance
 router.get("/users/attendance", UsersRestHandler.getAttendance);
