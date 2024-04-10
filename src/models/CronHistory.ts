6ac2c7a73a346c2787fc659c653587bc8b159af2
supersaaam <70166392+supersaaam@users.noreply.github.com>
2024-04-10 07:29:05 -0400

    addl. feat: Cron Validation + Save Attedance + Save Cron History

---
+++ src/models/CronHistory.ts
@@ -0,0 +1,24 @@
+import { DataTypes } from "sequelize";
+import { dbConnectCustomer } from "../database/connection";
+
+interface CronHistoryAttributes {
+    TrailID: number;
+    Date: String;
+}
+
+const CronHistory = dbConnectCustomer.define('CronHistory', {
+    TrailID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    Date: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+}, {
+    tableName: 'CronHistory',
+    timestamps: false // Disable auto-generating createdAt and updatedAt columns
+});
+
+export { CronHistory, CronHistoryAttributes };
